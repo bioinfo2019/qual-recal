8e3b92f5d838ebea9767c405879270caa52b949d
bioinfo2019 <eliotcline@gmail.com>
2020-01-27 10:59:47 +0700

    Delete bamlib.d

--- nsight_projects/bamlib/Debug/src/bamlib.d
+++
@@ -1,9 +0,0 @@
-src/bamlib.d: ../src/bamlib.cpp ../src/bamlib.hpp \
- /home/eliot/anaconda2/include/zlib.h \
- /home/eliot/anaconda2/include/zconf.h
-
-../src/bamlib.hpp:
-
-/home/eliot/anaconda2/include/zlib.h:
-
-/home/eliot/anaconda2/include/zconf.h:
