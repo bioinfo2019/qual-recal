383124a2680fb502182e1535ba1fd41a1fc72ebb
bioinfo2019 <eliotcline@gmail.com>
2020-06-27 12:51:51 +0700

    Update ModelPredict.py

--- Python/ModelPredict.py
+++ Python/ModelPredict.py
@@ -1,73 +1,49 @@
-#!/home/eliot/anaconda2/bin/python
+#!/path/to/anaconda3/bin/python
 
-import sys
+import argparse
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-from sklearn.externals import joblib
-import itertools
+import joblib
 import math
-from sklearn import metrics
 
-from collections import Counter
-
-from sklearn.model_selection import StratifiedKFold
-from sklearn.ensemble import AdaBoostClassifier
-from sklearn.tree import DecisionTreeClassifier
-from sklearn import ensemble
-from sklearn.linear_model import LogisticRegression
-from sklearn.svm import SVC
 from sklearn import preprocessing
-from sklearn.model_selection import train_test_split, GridSearchCV, cross_val_score
 from sklearn.metrics import confusion_matrix
 from sklearn.isotonic import IsotonicRegression as IR
 from sklearn.metrics import brier_score_loss, f1_score
-from sklearn.metrics import roc_curve
 from sklearn.metrics import recall_score, precision_score
-from sklearn.model_selection import RandomizedSearchCV
-from sklearn.utils import check_random_state, safe_indexing
-from sklearn.calibration import CalibratedClassifierCV, calibration_curve
 from sklearn.metrics import average_precision_score
 
-#from sklearn.externals.six import string_types
-from sklearn.model_selection import cross_val_predict
-from imblearn.under_sampling import TomekLinks
-from imblearn.metrics import geometric_mean_score
-
-# Set to be the same as the folder set in ModelTraining.py
-BASE_DIR = '/media/eliot/WD4TB/eliot_files/MyFiles/PhD'
-
-MAPQ = 3
-INSERT_SIZE = 4
-GAP_EXTENSIONS = 5
-EDIT_DISTANCE = 6
-MISMATCHES = 7
-GAP_OPENS = 8
-ALIGNMENT_SCORE = 9
-SECONDARY_ALIGNMENT_SCORE = 10
-READ_COMPLEXITY_SCORE = 11
-READ_GC_CONTENT = 12
-PAIR_ORIENTATION = 13
-PAIR_ALIGNMENT_TYPE = 14
-INTERCEPT = 15
-SLOPE = 16
-R_VALUE = 17
-DEPTH = 18
-READ_EIGENVAL_1 = 19
-READ_EIGENVAL_2 = 20
-READ_TRACE = 21
-READ_DET = 22
-REF_EIGENVAL_1 = 23
-REF_EIGENVAL_2 = 24
-REF_TRACE = 25
-REF_DET = 26
-REF_COMPLEXITY_SCORE = 27
-REF_GC_CONTENT = 28
-N_LOW_QUAL_BASES = 29
-AVG_LOW_QUAL_BASE_SCORE = 30
-MATE_ALIGNMENT_SCORE = 31
-ALIGNMENT_SCORES_DIFF = 32
-CLASS = 33
+feats = {'MAPQ':3,
+        'INSERT_SIZE':4,
+        'GAP_EXTENSIONS':5,
+        'EDIT_DISTANCE':6,
+        'MISMATCHES':7,
+        'GAP_OPENS':8,
+        'ALIGNMENT_SCORE':9,
+        'SECONDARY_ALIGNMENT_SCORE':10,
+        'READ_COMPLEXITY_SCORE':11,
+        'READ_GC_CONTENT':12,
+        'PAIR_ORIENTATION':13,
+        'PAIR_ALIGNMENT_TYPE':14,
+        'INTERCEPT':15,
+        'SLOPE':16,
+        'R_VALUE':17,
+        'DEPTH':18,
+        'READ_EIGENVAL_1':19,
+        'READ_EIGENVAL_2':20,
+        'READ_TRACE':21,
+        'READ_DET':22,
+        'REF_EIGENVAL_1':23,
+        'REF_EIGENVAL_2':24,
+        'REF_TRACE':25,
+        'REF_DET':26,
+        'REF_COMPLEXITY_SCORE':27,
+        'REF_GC_CONTENT':28,
+        'N_LOW_QUAL_BASES':29,
+        'AVG_LOW_QUAL_BASE_SCORE':30,
+        'MATE_ALIGNMENT_SCORE':31,
+        'ALIGNMENT_SCORES_DIFF':32,
+        'CLASS':33}
 
 
 def parallel_predict(X, est, n_chunks=8):
@@ -118,7 +94,7 @@ def parallel_decision_function(X, est, n_chunks=8):
     jobs = (joblib.delayed(est.decision_function)(array) for array in data_chunks)
     parallel = joblib.Parallel(n_jobs=n_chunks)
     
-    # Run jobs: works
+    # Run jobs: works150bp
     results = parallel(jobs)
     return np.asarray([y for x in results for y in x])
 
@@ -139,64 +115,82 @@ def write_scores_file(read_names, out_file_name, prob):
 #            phred = int(mapqs[i])
                 
         line = []
-        line.append(read_names[i][0])
+        line.append(read_names[i])
         line.append(phred)
 
         
         data.append(line)
-        
+
     #out.write(dataset[i] + '\t' + str(phred) + '\t' + str(prob[i]) + '\t' + str(1.0 - prob[i]) + '\t' + str(y_true[i]) + '\t' + str(y_pred[i]) + '\n')
     
     df = pd.DataFrame(data)
-    df.to_csv(out_file_name, sep='\t', header=False)
+    df.to_csv(out_file_name, sep='\t', header=False, index=False)
+
+################## Parse command line args #############################
     
-################## Run Parameters ######################################
+parser = argparse.ArgumentParser(description='Train model and write it to disk')
 
-feats_file = BASE_DIR + '/varsim_run/xaa'
+parser.add_argument('-b','--base-dir', help='Top level folder containing scripts and data', required=True)
+parser.add_argument('-m','--ml-model', help='Type of model to train', required=True)
+parser.add_argument('-f','--features', help='Comma separated list of features to be used for prediction', required=True)
+parser.add_argument('-v','--feats-file', help='TSV file containing feature vectors for prediction', required=True)
 
-#cols = [MAPQ,INSERT_SIZE,GAP_EXTENSIONS,EDIT_DISTANCE,MISMATCHES,GAP_OPENS,ALIGNMENT_SCORE,SECONDARY_ALIGNMENT_SCORE,PAIR_ORIENTATION,PAIR_ALIGNMENT_TYPE,INTERCEPT,SLOPE,N_LOW_QUAL_BASES,CLASS]
-cols = (SECONDARY_ALIGNMENT_SCORE,ALIGNMENT_SCORES_DIFF,ALIGNMENT_SCORE,MAPQ,MATE_ALIGNMENT_SCORE,MISMATCHES,EDIT_DISTANCE,CLASS)
+args = parser.parse_args()
 
-est_type = 'ADB'
+BASE_DIR = args.base_dir
+est_type = args.ml_model
+feats_file = BASE_DIR + '/models/' + args.feats_file
+feature_list = args.features
+
+cols = tuple([feats[key] for key in feature_list.split(',')])
 
 ########################################################################
 
 
 
-print "loading data file...."
+print("loading data file....")
 
-dataset = pd.read_csv(feats_file, usecols=cols, delimiter="\t").values
+d = pd.read_csv(feats_file, usecols=cols, delimiter="\t")
+#wrong_idxs = pd.read_csv('/hdd1/eliot_files/MyFiles/PhD/models/wrong_idxs.txt').iloc[:,0].to_list()
+#fps = d.iloc[wrong_idxs, :]
+#fps.to_csv(BASE_DIR + '/models/M82_1200K.wrong_feats.tab', header=False, index=False)
+#sys.exit(0)
+#d = d[d.MAPQ == 60]
+#d.drop(['MAPQ'], axis=1, inplace=True)
+
 read_names = pd.read_csv(feats_file, usecols=(0,), delimiter="\t").values
 
-print "done\n"        
+dataset = d.values
 
-last_col = len(cols) - 1
+print("done\n")    
+
+last_col = len(d.columns) - 1
 X = dataset[:,0:last_col]
 y = dataset[:,last_col]
 y = y.astype(int)
 
 #mapqs = X[:, 0]
-ir = IR( out_of_bounds = 'clip' )
+ir = IR()
 
 
-print "scaling input data...."
+print("scaling input data....")
 min_max_scaler = preprocessing.MinMaxScaler()
 Xs = min_max_scaler.fit_transform(X)
-print "done\n"
+print("done\n")
 
 X = 0
 
-print "Predicting and calibrating probabilities...\n"
+print("Predicting and calibrating probabilities...\n")
 
-clf = joblib.load(BASE_DIR + '/models/' + est_type + '.pkl')
-ir = joblib.load(BASE_DIR + '/models/' + est_type + '_ISO.pkl')
-ir = IR( out_of_bounds = 'clip' )
-#class_pred = parallel_predict(Xs, clf, 2)
-class_pred = clf.predict(Xs)
+clf = joblib.load(BASE_DIR + '/models/' + est_type + '.TOM046A.FULL.pkl')
+ir = joblib.load(BASE_DIR + '/models/' + est_type + '_ISO.TOM046A.FULL.pkl')
+
+class_pred = parallel_predict(Xs, clf, 8)
+#class_pred = clf.predict(Xs)
 
 if est_type != 'RUSVM' and est_type != 'RUSVMS':
-    #proba_pred = parallel_predict_proba(Xs, clf, 2)[:,1]
-    proba_pred = clf.predict_proba(Xs)[:,1]
+    proba_pred = parallel_predict_proba(Xs, clf, 8)[:,1]
+    #proba_pred = clf.predict_proba(Xs)[:,1]
     p_cal = ir.transform(proba_pred)
 else:
     distance = clf.decision_function(Xs)
@@ -212,86 +206,57 @@ test_preds_cal = p_cal > 0.5
 test_preds_cal = test_preds_cal.astype(int)
 cm_preds_cal = confusion_matrix(y, test_preds_cal)
 
+#wrong = (test_preds_cal != y).nonzero()
+#np.savetxt("/hdd1/eliot_files/MyFiles/PhD/models/wrong_idxs.txt", wrong, fmt='%d', delimiter='\n')
 
-print "Test stats (no recal):"
-print "Confusion matrix:"
-print cm_preds_no_cal
-print "precision:"
-print precision_score(y, class_pred, average='micro')
-print "Average precision:"
+print("Test stats (no recal):")
+print("Confusion matrix:")
+print(cm_preds_no_cal)
+print("precision:")
+print(precision_score(y, class_pred, average='micro'))
+print("Average precision:")
 if est_type == 'RUSVM' or est_type == 'RUSVMS':
-    print average_precision_score(y, distance, average='weighted')
+    print(average_precision_score(y, distance, average='weighted'))
 else:
-    print average_precision_score(y, proba_pred, average='weighted') 
+    print(average_precision_score(y, proba_pred, average='weighted'))
 
 
-print "recall:"
-print recall_score(y, class_pred, average='micro')
-print "gmean:"
-print geometric_mean_score(y, class_pred, average='weighted')
-print "F1 score:"
-print f1_score(y, class_pred)
-print "Brier score:"
-print brier_score_loss(y, proba_pred)
+print("recall:")
+print(recall_score(y, class_pred, average='micro'))
+print("gmean:")
+#print(geometric_mean_score(y, class_pred, average='weighted'))
+print("F1 score:")
+print(f1_score(y, class_pred))
+print("Brier score:")
+print(brier_score_loss(y, proba_pred))
 
 
-print ""
+print("")
 
-print "Test stats (recal):"
-print "Confusion matrix:"
-print cm_preds_cal
-print "precision:"
-print precision_score(y, test_preds_cal, average='micro')
-print "Average precision:"
-print average_precision_score(y, p_cal, average='weighted') 
-print "recall:"
-print recall_score(y, test_preds_cal, average=None)
-print "gmean:"
-print geometric_mean_score(y, test_preds_cal, average=None)
-print "gmean (weighted):"
-print geometric_mean_score(y, test_preds_cal, average='weighted')
-print "F1 score:"
-print f1_score(y, class_pred)
-print "Brier score:"
-print brier_score_loss(y, p_cal)
+print("Test stats (recal):")
+print("Confusion matrix:")
+print(cm_preds_cal)
+print("precision:")
+print(precision_score(y, test_preds_cal, average='micro'))
+print("Average precision:")
+print(average_precision_score(y, p_cal, average='weighted'))
+print("recall:")
+print(recall_score(y, test_preds_cal, average=None))
+print("gmean:")
+#print(geometric_mean_score(y, test_preds_cal, average=None))
+print("gmean (weighted):")
+#print(geometric_mean_score(y, test_preds_cal, average='weighted'))
+print("F1 score:")
+print(f1_score(y, class_pred))
+print("Brier score:")
+print(brier_score_loss(y, p_cal))
 
-print "\nWriting scores file..."
+print("\nWriting scores file...")
+
 
 if est_type == 'RUSVM' or est_type == 'RUSVMS':
-    write_scores_file(read_names, BASE_DIR + '/pepper_scores_' + est_type + '_1.tab', proba_pred)
+    write_scores_file(read_names, BASE_DIR + '/models/scores.tab', proba_pred)
 else:
-    write_scores_file(read_names, BASE_DIR + '/pepper_scores_' + est_type + '_1.tab', p_cal) 
-    
-print "Done!"
+    write_scores_file(read_names, BASE_DIR + '/models/scores.tab', p_cal) 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print("Done!")
